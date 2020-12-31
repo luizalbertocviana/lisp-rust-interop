3307f40754b8dee9445f641038f7d5fd5b6a41dd
Luiz Alberto <luizalbertocviana@gmail.com>
2020-12-30 22:58:20 -0300

    major changes: struct does not work right

--- rust-lib/lib.lisp
+++ rust-lib/lib.lisp
@@ -5,6 +5,23 @@
 
 (load-shared-object "target/release/librust_lisp_interop.so")
 
-(define-alien-routine "add" int
-  (x int)
-  (y int))
+(define-alien-routine "add" long
+  (x long)
+  (y long))
+
+(define-alien-routine "hello" c-string)
+
+(define-alien-type nil
+    (struct Point
+            (x long)
+            (y long)))
+
+(define-alien-routine "new_point" (* (struct Point))
+  (x long)
+  (y long))
+
+(define-alien-routine "get_x" long
+  (point (* (struct Point))))
+
+(define-alien-routine "get_y" long
+  (point (* (struct Point))))
--- rust-lib/src/lib.rs
+++ rust-lib/src/lib.rs
@@ -3,10 +3,54 @@ pub extern "C" fn add(x: i64, y: i64) -> i64 {
     x + y
 }
 
+#[no_mangle]
+pub extern "C" fn hello() -> *const u8 {
+    "hello from rust\0".as_ptr()
+}
+
+#[repr(C)]
+pub struct Point {
+    x: i64,
+    y: i64,
+}
+
+impl Point {
+    fn new(x: i64, y: i64) -> Self {
+        Self {x, y}
+    }
+    fn get_x(&self) -> i64 {
+        self.x
+    }
+    fn get_y(&self) -> i64 {
+        self.y
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn new_point(x: i64, y: i64) -> *const Point {
+    &Point::new(x, y)
+}
+
+#[no_mangle]
+pub extern "C" fn get_x(p: *const Point) -> i64 {
+    unsafe {
+        p.as_ref().expect("invalid pointer").get_x()
+    }
+}
+
+#[no_mangle]
+pub extern "C" fn get_y(p: *const Point) -> i64 {
+    unsafe {
+        p.as_ref().expect("invalid pointer").get_y()
+    }
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+    
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        assert_eq!(4, add(2, 2));
     }
 }
