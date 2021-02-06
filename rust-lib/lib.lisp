4cc3c09fe159ff1d1fc11713f9a74d793b12f81b
Luiz Alberto <luizalbertocviana@gmail.com>
2021-02-06 16:26:45 -0300

    removed functions not related with Point (now opaque)

--- rust-lib/lib.lisp
+++ rust-lib/lib.lisp
@@ -6,16 +6,7 @@
 
 (load-shared-object "target/release/librust_lisp_interop.so")
 
-(define-alien-routine "add" long
-  (x long)
-  (y long))
-
-(define-alien-routine "hello" c-string)
-
-(define-alien-type nil
-    (struct Point
-            (x long)
-            (y long)))
+(define-alien-type nil (struct Point))
 
 (define-alien-routine "new_point" (* (struct Point))
   (x long)
